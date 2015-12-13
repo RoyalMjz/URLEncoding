410aa503926c192afa7663df2b6212e8f3b7b388
RoyalMjz <royal_mjz@163.com>
2015-12-13 12:20:40 +0800

    请求的数据有汉字

--- URLEncoding/ViewController.h
+++ URLEncoding/ViewController.h
@@ -3,7 +3,7 @@
 //  URLEncoding
 //
 //  Created by Royal丶MJZ on 15/12/13.
-//  Copyright © 2015年 mjz. All rights reserved.
+//  Copyright (c) 2015年 mjz. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- URLEncoding/ViewController.m
+++ URLEncoding/ViewController.m
@@ -3,10 +3,11 @@
 //  URLEncoding
 //
 //  Created by Royal丶MJZ on 15/12/13.
-//  Copyright © 2015年 mjz. All rights reserved.
+//  Copyright (c) 2015年 mjz. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "NSString+URLEncoding.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //1.路径：通过GET方式获取接口
+    NSString *path = [@"http://www.webxml.com.cn/WebServices/WeatherWebService.asmx/getWeatherbyCityName?theCityName=沈阳" URLEncodedString];
+    //2.转成URL
+    NSURL *url = [NSURL URLWithString:path];
+    //3.建立请求
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    //4.通过block请求数据
+    //创建队列
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [NSURLConnection sendAsynchronousRequest:request queue:queue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        if (connectionError) {
+            NSLog(@"下载失败");
+        }
+        //回到主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"data:%@", data);
+        });
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,3 +44,8 @@
 }
 
 @end
+
+
+
+
+
